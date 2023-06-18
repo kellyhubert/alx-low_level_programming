8ad46e3c21b1fca3e308c3ee289fdc35bdcf365c
kellyhubert <irakellyhub@gmail.com>
2023-06-18 09:45:53 -0700

    prints (programming is like building a multilingual puzzel)

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ * main prints out outputs using function puts
+ *
+ * always return (0)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzel\n");
+	return (0);
+}
